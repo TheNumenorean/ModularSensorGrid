ddae6e9ecee5d625ec0fb36d42a242f756eabfef
Francesco Macagno <fjmacagno1@yahoo.com>
2014-06-18 15:59:20 -0700

    Implemented ArdulinkConnector
    Still needs a little work

--- java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/usb/ardulink/ArdulinkConnector.java
+++ java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/usb/ardulink/ArdulinkConnector.java
@@ -1,89 +1,111 @@
 package net.thenumenorean.modularsensorgrid.usb.ardulink;
 
-import org.zu.ardulink.Link;
+import java.util.ArrayList;
 
 import net.thenumenorean.modularsensorgrid.connector.AnalogChangeListener;
 import net.thenumenorean.modularsensorgrid.connector.Connector;
 import net.thenumenorean.modularsensorgrid.connector.DigitalChangeListener;
 
+import org.zu.ardulink.Link;
+import org.zu.ardulink.event.AnalogReadChangeEvent;
+import org.zu.ardulink.event.AnalogReadChangeListener;
+import org.zu.ardulink.event.DigitalReadChangeEvent;
+import org.zu.ardulink.event.DigitalReadChangeListener;
+import org.zu.ardulink.protocol.IProtocol;
+
 public class ArdulinkConnector implements Connector {
-	
+
 	private String port;
 	private Link link;
-	
-	public ArdulinkConnector(Link l, String port){
+	private long startTime; // Temporary TODO
+	private ArrayList<AnalogListenerConverter> analogListeners;
+	private ArrayList<DigitalListenerConverter> digitalListeners;
+
+	public ArdulinkConnector(Link l, String port) {
 		this.port = port;
 		link = l;
-		//TODO:
+		analogListeners = new ArrayList<AnalogListenerConverter>();
+		// TODO:
 	}
-	
-	public String getPort(){
+
+	public String getPort() {
 		return port;
 	}
 
 	@Override
 	public void setDigitalPin(int pin, boolean on) {
-		// TODO Auto-generated method stub
-
+		link.sendPowerPinSwitch(pin, on ? IProtocol.HIGH : IProtocol.LOW);
 	}
 
 	@Override
 	public void setPWM(int pin, int value) {
-		// TODO Auto-generated method stub
-
+		link.sendPowerPinIntensity(pin, value);
 	}
 
 	@Override
 	public void setTimeZero() {
-		// TODO Auto-generated method stub
-
+		link.sendCustomMessage("resetTime");
+		startTime = System.currentTimeMillis(); // TODO
 	}
 
 	@Override
 	public void setReportAnalogChanges(int pin, boolean report) {
-		// TODO Auto-generated method stub
-
+		if (report)
+			link.startListenAnalogPin(pin);
+		else
+			link.stopListenAnalogPin(pin);
 	}
 
 	@Override
-	public void addAnalogChangeListener(AnalogChangeListener acl) {
-		// TODO Auto-generated method stub
+	public void addAnalogChangeListener(final AnalogChangeListener acl) {
+
+		AnalogListenerConverter conv = new AnalogListenerConverter(acl);
+		analogListeners.add(conv);
+		link.addAnalogReadChangeListener(conv);
 
 	}
 
 	@Override
 	public void removeAnalogChangeListener(AnalogChangeListener acl) {
-		// TODO Auto-generated method stub
+		for (AnalogListenerConverter alc : analogListeners)
+			if (alc.getAnalogChangeListener().equals(acl))
+				link.removeAnalogReadChangeListener(alc);
 
 	}
 
 	@Override
 	public void clearAnalogChangeListeners() {
-		// TODO Auto-generated method stub
-
+		for (AnalogListenerConverter alc : analogListeners)
+			link.removeAnalogReadChangeListener(alc);
 	}
 
 	@Override
 	public void setReportDigitalChanges(int pin, boolean report) {
-		// TODO Auto-generated method stub
-
+		if (report)
+			link.startListenDigitalPin(pin);
+		else
+			link.stopListenDigitalPin(pin);
 	}
 
 	@Override
 	public void addDigitalChangeListener(DigitalChangeListener dcl) {
-		// TODO Auto-generated method stub
-
+		DigitalListenerConverter conv = new DigitalListenerConverter(dcl);
+		digitalListeners.add(conv);
+		link.addDigitalReadChangeListener(conv);
 	}
 
 	@Override
 	public void removeDigitalChangeListener(DigitalChangeListener dcl) {
-		// TODO Auto-generated method stub
+		for (DigitalListenerConverter dlc : digitalListeners)
+			if (dlc.getDigitalChangeListener().equals(dcl))
+				link.removeDigitalReadChangeListener(dlc);
 
 	}
 
 	@Override
 	public void clearDigitalChangeListeners() {
-		// TODO Auto-generated method stub
+		for (DigitalListenerConverter dlc : digitalListeners)
+			link.removeDigitalReadChangeListener(dlc);
 
 	}
 
@@ -91,6 +113,59 @@ public class ArdulinkConnector implements Connector {
 	public void destroy() {
 		link.disconnect();
 		Link.destroyInstance(link.getName());
+	}
+
+	private class AnalogListenerConverter implements AnalogReadChangeListener {
+
+		private AnalogChangeListener acl;
+
+		public AnalogListenerConverter(AnalogChangeListener acl) {
+			this.acl = acl;
+		}
+
+		public AnalogChangeListener getAnalogChangeListener() {
+			return acl;
+		}
+
+		@Override
+		public int getPinListening() {
+			return AnalogReadChangeListener.ALL_PINS;
+		}
+
+		@Override
+		public void stateChanged(AnalogReadChangeEvent arg0) {
+			acl.onAnalogChange(arg0.getPin(), arg0.getValue(),
+					System.currentTimeMillis() - startTime); // TODO
+
+		}
+
+	}
+
+	private class DigitalListenerConverter implements DigitalReadChangeListener {
+
+		private DigitalChangeListener dcl;
+
+		public DigitalListenerConverter(DigitalChangeListener dcl) {
+			this.dcl = dcl;
+		}
+
+		public DigitalChangeListener getDigitalChangeListener() {
+			return dcl;
+		}
+
+		@Override
+		public int getPinListening() {
+			return AnalogReadChangeListener.ALL_PINS;
+		}
+
+		@Override
+		public void stateChanged(DigitalReadChangeEvent arg0) {
+			dcl.onDigitalChange(arg0.getPin(),
+					arg0.getValue() == IProtocol.HIGH,
+					System.currentTimeMillis() - startTime); // TODO
+
+		}
+
 	}
 
 }
--- java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/usb/ardulink/ArdulinkGridPopulatorFrame.java
+++ java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/usb/ardulink/ArdulinkGridPopulatorFrame.java
@@ -1,6 +1,5 @@
 package net.thenumenorean.modularsensorgrid.usb.ardulink;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
