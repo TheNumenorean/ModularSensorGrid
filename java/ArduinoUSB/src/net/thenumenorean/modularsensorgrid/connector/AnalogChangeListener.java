698c6e97ba7e8de2f5c67ff9750c8fde803e69da
Francesco Macagno <fjmacagno1@yahoo.com>
2014-06-19 14:03:18 -0700

    Git is being stupid

--- java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/connector/AnalogChangeListener.java
+++
@@ -1,7 +0,0 @@
-package net.thenumenorean.modularsensorgrid.connector;
-
-public interface AnalogChangeListener {
-	
-	public void onAnalogChange(int pin, int value, long timestamp);
-
-}
--- java/ArduinoUSB/src/net/thenumenorean/modularsensorgrid/connector/DigitalChangeListener.java
+++
@@ -1,7 +0,0 @@
-package net.thenumenorean.modularsensorgrid.connector;
-
-public interface DigitalChangeListener {
-	
-	public void onDigitalChange(int pin, boolean value, long timestamp);
-
-}
